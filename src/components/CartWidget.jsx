7e367adf686bd59827081f23b2d6e9f4c0cef11b
Mauro Largo <largomaurondres@hotmail.com>
2023-03-06 17:42:27 -0300

    agregar componentes

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+        <span className ="material-symbols-outlined">
+            shopping_cart <span>5</span>
+        </span>
+        
+    </div>
+  )
+}
+
+export default CartWidget
